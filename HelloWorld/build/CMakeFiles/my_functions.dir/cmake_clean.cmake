c769b085233d5de938c0c922a83e02e1dba18460
rajathande <rajathande@outlook.com>
2025-07-26 18:12:55 +0900

    Update README.md with detailed build and test instructions

---
+++ HelloWorld/build/CMakeFiles/my_functions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_functions.dir/src/functions.cpp.o"
+  "CMakeFiles/my_functions.dir/src/functions.cpp.o.d"
+  "libmy_functions.a"
+  "libmy_functions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HelloWorld/build/CMakeFiles/my_functions.dir/src/functions.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/my_functions.dir/src/functions.cpp.o: \
+ /home/rajat/vector/HelloWorld/src/functions.cpp \
+ /usr/include/stdc-predef.h \
+ /home/rajat/vector/HelloWorld/include/functions.h
